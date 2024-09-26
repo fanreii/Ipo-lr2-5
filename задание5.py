f53d66826b6f55bd29eac069b7134c9e0ba75102
86 <katerinaankuda@gmail.com>
2024-09-26 18:14:37 +0300

    sd

---
+++ задание5.py
@@ -0,0 +1,10 @@
+import math
+S = float
+a = float(input("Введите значение длины 1 основания: "))
+b = float(input("Введите значение длины 2 основания: "))
+c = float(input("Значение боковой стороны: "))
+d = float(input("Значение второй боковой стороны: "))
+p = float 
+p = (a + b + c + d) / 2
+S = ((a + b)/ math.fabs(a - b)) * math.sqrt((p-a)*(p-b)*(p-a-c)*(p-a-d))
+print("Площадь равна:", S)
